3e59bfec2f1f37ff6de8ea1ba111272d569718a3
luizotaviopdev <orviorze@gmail.com>
2025-05-10 19:28:24 -0300

    desafios e exercicios

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,3 @@
+var vel =  90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 80){ console.log(`Você Ultrapassou a velocidade permitida!`); console.log(`Você será multado no valor de ${(vel-80) * 5}R$`)}  else { console.log(`Dirija sempre usando cinto de segurança!`) }
